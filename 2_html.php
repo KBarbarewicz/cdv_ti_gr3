38f9dbfe6d52769b4324c0b9d73c894e3b1300e6
KBarbarewicz <79199623+KBarbarewicz@users.noreply.github.com>
2023-04-12 16:38:35 +0200

    errors fix 12-4-23
    all things should works now

--- 2_html.php
+++ 2_html.php
@@ -20,25 +20,24 @@
 		<li>Gniezno</li>
 		<li>Jarocin</li>
 	</ul>
-<?php
-$city = "Poznań";
-echo <<< LIST
-<hr>HEREDOC<br>
 
-<h4>Lista</h4>
-<ul>
-    <li>
-        $city
-        <ol type="A">
-            <li>Polna</li>
-            <li>Gnieźnieńska</li>
-        </ol>
-    </li>
-    <li>Gniezno</li>
-    <li>Jarocin</li>
-</ul>
+    <?php
+        $city = "Poznań";
+        echo <<< LIST
+            <hr>HEREDOC<br>
+            <ul>
+                <li>
+                    $city
+                    <ol type="A">
+                        <li>Polna</li>
+                        <li>Gnieźnieńska</li>
+                    </ol>
+                </li>
+                <li>Gniezno</li>
+                <li>Jarocin</li>
+	        </ul>
 LIST;
-?>
 
+    ?>
 </body>
 </html>
