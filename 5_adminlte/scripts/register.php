5610730b719e8b7073fefb2a42ef8062d29d5138
KBarbarewicz <79199623+KBarbarewicz@users.noreply.github.com>
2023-04-26 20:47:45 +0200

    update to working version
    there is not password hash

--- 5_adminlte/scripts/register.php
+++ 5_adminlte/scripts/register.php
@@ -1,4 +1,14 @@
 <?php
-echo "<pre>";
-print_r($_POST);
-echo "</pre>";
+//echo "<pre>";
+//print_r($_POST);
+//echo "</pre>";
+foreach ($_POST as $key => $value){
+    if (empty($value)){
+        echo "<script>history.back();</script>";
+        exit();
+    }
+}
+
+require_once "./connect.php";
+$sql = "INSERT INTO `users` (`city_id`, `firstName`, `lastName`, `birthday`, `email`, `password`) VALUES ('$_POST[city_id]', '$_POST[firstName]', '$_POST[lastName]', '$_POST[birthday]', '$_POST[email]', '$_POST[password]');";
+$conn->query($sql);
